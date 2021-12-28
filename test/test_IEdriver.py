31fd022f2a9d51e8664a8018037973198c8ffb38
sayar mendis <sayarmendis26@gmail.com>
2021-12-28 12:06:09 +0530

    fix test cases

--- test/test_IEdriver.py
+++ test/test_IEdriver.py
@@ -2,6 +2,8 @@ import os
 import sys
 import time
 
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 DATA_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.abspath(os.path.dirname(DATA_DIR)))
 
@@ -13,10 +15,12 @@ iedriver_path = os.path.abspath(DATA_DIR + '/IEDriverServer.exe')
 
 def test_IE():
     sc = Screenshot()
-    driver = webdriver.Ie(executable_path=iedriver_path)
-    url = 'http://yandex.ru'
+    driver = webdriver.Edge(EdgeChromiumDriverManager(log_level=0).install())
+    url = 'https://github.com/PyWizards/Selenium_Screenshot'
     driver.get(url)
     time.sleep(10)
-    sc.full_Screenshot(driver, save_path='.', image_name='testimage.png',load_wait_time=5,is_load_at_runtime=True)
+    image_path = sc.full_Screenshot(driver, save_path='.', image_name='testimage.png',load_wait_time=5,is_load_at_runtime=True)
+    os.remove(image_path)
     driver.close()
+
     driver.quit()
--- test/test_chrome_driver.py
+++ test/test_chrome_driver.py
@@ -6,45 +6,40 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(DATA_DIR)))
 
 from selenium import webdriver
 from Screenshot import Screenshot_Clipping
-
-chromedriver_path = os.path.abspath(DATA_DIR + '/chromedriver.exe')
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_full_screenshot():
     ob = Screenshot_Clipping.Screenshot()
-    driver = webdriver.Chrome(executable_path=chromedriver_path)
-    # url = "https://github.com/sam4u3/Selenium_Screenshot/tree/master/test"
-    url = 'http://yandex.ru'
+    driver = webdriver.Chrome(ChromeDriverManager(log_level=0).install())
+    url = "https://github.com/sam4u3/Selenium_Screenshot/tree/master/test"
     driver.get(url)
     img_url = ob.full_Screenshot(driver, save_path=r'.',image_name='Myimage.png',is_load_at_runtime=True,load_wait_time=3)
     os.remove(img_url)
     driver.close()
-
     driver.quit()
 
 
 def test_element_screenshot():
     ob = Screenshot_Clipping.Screenshot()
-    driver = webdriver.Chrome(executable_path=chromedriver_path)
-    url = "https://github.com/sam4u3/Selenium_Screenshot/blob/master/Screenshot/Screenshot_Clipping.py"
+    driver = webdriver.Chrome(ChromeDriverManager(log_level=0).install())
+    url = "https://github.com/PyWizards/Selenium_Screenshot"
     driver.get(url)
-
-    element = driver.find_element_by_class_name('signup-prompt')
+    element = driver.find_element_by_class_name('pagehead-actions')
     img_url = ob.get_element(driver, element, r'.')
     os.remove(img_url)
-
     driver.close()
-
     driver.quit()
 
 
 def test_hide_element():
     ob = Screenshot_Clipping.Screenshot()
-    driver = webdriver.Chrome(executable_path=chromedriver_path)
+    driver = webdriver.Chrome(ChromeDriverManager(log_level=0).install())
     url = "https://github.com/sam4u3"
     driver.get(url)
-    Hide_elements = ['class=avatar width-full height-full avatar-before-user-status']  # Use full class name
-    img_url = ob.full_Screenshot(driver, save_path=r'.', elements=Hide_elements,
+    hide_elements = ['class=avatar width-full height-full avatar-before-user-status']  # Use full class name
+    img_url = ob.full_Screenshot(driver, save_path=r'.', elements=hide_elements,
                                  image_name='Myimage.png')
     os.remove(img_url)
     driver.close()
